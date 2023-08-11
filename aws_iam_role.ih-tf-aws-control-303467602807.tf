2086ce54842135658e29aea7ef8a2a7b64461522
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-11 11:03:05 -0700

    Add role ih-tf-aws-control-303467602807-read-only
    We need the read-only role so it can read hosted zone
    ci-cd.infrahouse.com

--- aws_iam_role.ih-tf-aws-control-303467602807.tf
+++ aws_iam_role.ih-tf-aws-control-303467602807.tf
@@ -9,3 +9,30 @@ module "ih-tf-aws-control-303467602807-admin" {
   repo_name                = "aws-control-303467602807"
   state_bucket             = "infrahouse-aws-control-303467602807"
 }
+
+
+data "aws_iam_policy_document" "ih-tf-aws-control-303467602807-read-only-assume" {
+  statement {
+    sid     = "000"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::990466748045:user/aleks",
+        "arn:aws:iam::493370826424:role/ih-tf-aws-control-493370826424-github"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ih-tf-aws-control-303467602807-read-only" {
+  provider           = aws.aws-303467602807-uw1
+  name               = "ih-tf-aws-control-303467602807-read-only"
+  description        = "Role to provide read-only access to terraform in 493370826424 account"
+  assume_role_policy = data.aws_iam_policy_document.ih-tf-aws-control-303467602807-read-only-assume.json
+}
+resource "aws_iam_role_policy_attachment" "ih-tf-aws-control-303467602807-read-only" {
+  provider   = aws.aws-303467602807-uw1
+  policy_arn = data.aws_iam_policy.read-only-access.arn
+  role       = aws_iam_role.ih-tf-aws-control-303467602807-read-only.name
+}
--- data_sources.tf
+++ data_sources.tf
@@ -2,3 +2,8 @@ data "aws_iam_policy" "administrator-access" {
   provider = aws.aws-303467602807-uw1
   name     = "AdministratorAccess"
 }
+
+data "aws_iam_policy" "read-only-access" {
+  provider = aws.aws-303467602807-uw1
+  name     = "ReadOnlyAccess"
+}
