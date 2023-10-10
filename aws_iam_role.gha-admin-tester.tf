3c791db96218fac7a81b09ce9cd20bf6b40ff5aa
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-10-10 11:37:53 -0700

    Add gha-admin-tester permissions
    Allow "s3:HeadBucket". Discovered with
    
    ```
    $ ih-plan min-permissions --existing-actions apply.json tf-apply-trace.txt
    ```

--- aws_iam_role.gha-admin-tester.tf
+++ aws_iam_role.gha-admin-tester.tf
@@ -63,6 +63,7 @@ data "aws_iam_policy_document" "gha-admin-tester-permissions" {
       "iam:TagRole",
       "s3:CreateBucket",
       "s3:DeleteBucket",
+      "s3:HeadBucket",
       "s3:PutBucketTagging",
       "dynamodb:CreateTable",
       "dynamodb:DeleteTable",
