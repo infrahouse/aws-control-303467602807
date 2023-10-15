bac4a4200d3d7f0c1eb078ecc74ec6f9d4e6e721
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-10-15 12:49:30 -0700

    Pin version of 'infrahouse/gha-admin/aws'

--- aws_iam_role.ih-tf-aws-control-303467602807.tf
+++ aws_iam_role.ih-tf-aws-control-303467602807.tf
@@ -2,10 +2,9 @@
 
 module "ih-tf-aws-control-303467602807-admin" {
   source  = "infrahouse/gha-admin/aws"
-  version = "~> 3.0"
+  version = "~> 1.0"
   providers = {
-    aws      = aws.aws-303467602807-uw1
-    aws.cicd = aws.aws-303467602807-uw1
+    aws = aws.aws-303467602807-uw1
   }
   gh_identity_provider_arn = module.github-connector.gh_openid_connect_provider_arn
   repo_name                = "aws-control-303467602807"
