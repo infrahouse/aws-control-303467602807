382e6397020c55aaf6c94ae69cb7ab5fa43d203f
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-10-15 12:44:52 -0700

    Pass provider to new gha-admin version

--- aws_iam_role.ih-tf-aws-control-303467602807.tf
+++ aws_iam_role.ih-tf-aws-control-303467602807.tf
@@ -1,9 +1,11 @@
 # Roles for CI/CD in the aws-control-303467602807 repo
 
 module "ih-tf-aws-control-303467602807-admin" {
-  source = "infrahouse/gha-admin/aws"
+  source  = "infrahouse/gha-admin/aws"
+  version = "~> 3.0"
   providers = {
-    aws = aws.aws-303467602807-uw1
+    aws      = aws.aws-303467602807-uw1
+    aws.cicd = aws.aws-303467602807-uw1
   }
   gh_identity_provider_arn = module.github-connector.gh_openid_connect_provider_arn
   repo_name                = "aws-control-303467602807"
