fcc2db3bc8a1b844b5cbd802f80a781d22a6a640
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-04-10 08:23:46 -0700

    Add role actions-runner-tester

---
+++ aws_iam_role.actions-runner-tester.tf
@@ -0,0 +1,16 @@
+module "actions-runner-tester" {
+  source = "./modules/module-tester-role"
+  providers = {
+    aws = aws.aws-303467602807-uw1
+  }
+  gh_org_name = "infrahouse"
+  repo_name   = "terraform-aws-actions-runner"
+  role_name   = "actions-runner-tester"
+  trusted_iam_user_arn = {
+    "me" : local.me_arn
+  }
+  role_permissions = [
+    "sts:GetCallerIdentity"
+  ]
+  grant_admin_permissions = true
+}
