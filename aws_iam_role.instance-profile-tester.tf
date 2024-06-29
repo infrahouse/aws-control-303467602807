57f836fe57f24519ba0c9b971871899cc4241eba
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-06-29 07:37:39 -0700

    Add role instance-profile-tester (#107)

---
+++ aws_iam_role.instance-profile-tester.tf
@@ -0,0 +1,14 @@
+module "instance-profile-tester" {
+  source = "./modules/module-tester-role"
+  providers = {
+    aws = aws.aws-303467602807-uw1
+  }
+  gh_org_name = "infrahouse"
+  repo_name   = "terraform-aws-instance-profile"
+  role_name   = "instance-profile-tester"
+  trusted_iam_user_arn = {
+    "me" : local.me_arn
+  }
+  role_permissions        = []
+  grant_admin_permissions = true
+}
