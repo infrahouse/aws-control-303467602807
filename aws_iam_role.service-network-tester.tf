66ba66dc6bbd4c07cbee694b9879f4d71c7a41e3
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-11-06 15:11:38 -0800

    Assign admin permissions to service-network-tester

--- aws_iam_role.service-network-tester.tf
+++ aws_iam_role.service-network-tester.tf
@@ -1,14 +1,128 @@
-module "service-network-tester" {
-  source = "./modules/module-tester-role"
-  providers = {
-    aws = aws.aws-303467602807-uw1
+locals {
+  service_network_tester_role_name = "service-network-tester"
+}
+
+## Data Sources
+
+data "aws_iam_policy_document" "service-network-tester-assume" {
+  statement {
+    sid     = "000"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::990466748045:user/aleks",
+        "arn:aws:iam::303467602807:role/${local.service_network_tester_role_name}"
+      ]
+    }
   }
-  gh_org_name = "infrahouse"
-  repo_name   = "terraform-aws-service-network"
-  role_name   = "service-network-tester"
-  trusted_iam_user_arn = {
-    "me" : local.me_arn
+  statement {
+    sid     = "010"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type = "Federated"
+      identifiers = [
+        module.github-connector.gh_openid_connect_provider_arn
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:infrahouse/terraform-aws-service-network:*"
+      ]
+    }
   }
-  role_permissions        = []
-  grant_admin_permissions = true
+}
+
+data "aws_iam_policy_document" "service-network-tester-permissions" {
+  statement {
+    actions = [
+      "ec2:AllocateAddress",
+      "ec2:AssociateRouteTable",
+      "ec2:AttachInternetGateway",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:CreateInternetGateway",
+      "ec2:CreateNatGateway",
+      "ec2:CreateRoute",
+      "ec2:CreateRouteTable",
+      "ec2:CreateSubnet",
+      "ec2:CreateTags",
+      "ec2:CreateVpc",
+      "ec2:DeleteInternetGateway",
+      "ec2:DeleteNatGateway",
+      "ec2:DeleteRoute",
+      "ec2:DeleteRouteTable",
+      "ec2:DeleteSubnet",
+      "ec2:DeleteVpc",
+      "ec2:DescribeAddresses",
+      "ec2:DescribeInternetGateways",
+      "ec2:DescribeNatGateways",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroupRules",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeVpcClassicLink",
+      "ec2:DescribeVpcClassicLinkDnsSupport",
+      "ec2:DescribeVpcs",
+      "ec2:DetachInternetGateway",
+      "ec2:DisassociateAddress",
+      "ec2:DisassociateRouteTable",
+      "ec2:ModifySubnetAttribute",
+      "ec2:ReleaseAddress",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RevokeSecurityGroupIngress",
+      "sts:AssumeRole",
+      "sts:GetCallerIdentity"
+    ]
+    resources = ["*"]
+  }
+}
+
+## EOF Data Sources
+
+# IAM policy
+
+resource "aws_iam_policy" "service-network-tester-permissions" {
+  provider = aws.aws-303467602807-uw1
+  name     = "${local.service_network_tester_role_name}-permissions"
+  policy   = data.aws_iam_policy_document.service-network-tester-permissions.json
+}
+
+# IAM role
+
+resource "aws_iam_role" "service-network-tester" {
+  provider           = aws.aws-303467602807-uw1
+  name               = local.service_network_tester_role_name
+  description        = "Role to test module terraform-aws-service-network"
+  assume_role_policy = data.aws_iam_policy_document.service-network-tester-assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "service-network-tester" {
+  provider   = aws.aws-303467602807-uw1
+  policy_arn = aws_iam_policy.service-network-tester-permissions.arn
+  role       = aws_iam_role.service-network-tester.name
+}
+
+resource "aws_iam_role_policy_attachment" "service-network-tester-pytest-permissions" {
+  provider   = aws.aws-303467602807-uw1
+  policy_arn = aws_iam_policy.pytest-permissions.arn
+  role       = aws_iam_role.service-network-tester.name
+}
+
+resource "aws_iam_role_policy_attachment" "service-network-tester-admin-permissions" {
+  provider   = aws.aws-303467602807-uw1
+  policy_arn = data.aws_iam_policy.administrator-access.arn
+  role       = aws_iam_role.service-network-tester.name
 }
