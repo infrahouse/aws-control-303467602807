c3a0b892bb2b543a8ec98cc2a6c4f967ef69d467
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-03-23 20:17:27 -0700

    Create GHA on Ubuntu noble (#145)

--- gha_runner.tf
+++ gha_runner.tf
@@ -34,3 +34,30 @@ module "actions-runner" {
     "npm",
   ]
 }
+
+module "actions-runner-noble" {
+  source  = "registry.infrahouse.com/infrahouse/actions-runner/aws"
+  version = "~> 2.5"
+
+  environment                = local.environment
+  github_org_name            = "infrahouse"
+  github_app_id              = 1016363
+  github_app_pem_secret_arn  = data.aws_secretsmanager_secret.github-terraform-app-key.arn
+  subnet_ids                 = module.management.subnet_public_ids
+  role_name                  = local.github_actions_role
+  instance_type              = "t3a.small"
+  root_volume_size           = 64
+  max_instance_lifetime_days = 5
+  asg_min_size               = 1
+  asg_max_size               = 1
+  on_demand_base_capacity    = 0
+  ubuntu_codename            = "noble"
+  extra_labels               = ["oracular"]
+  puppet_hiera_config_path   = "/opt/infrahouse-puppet-data/environments/${local.environment}/hiera.yaml"
+  packages = [
+    "debhelper",
+    "devscripts",
+    "infrahouse-puppet-data",
+    "golang",
+  ]
+}
