4bb3b7455257f38de13c2cba486f7e62002d8616
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-05-18 14:55:32 -0700

    Remove one NAT gateway

--- service-network.tf
+++ service-network.tf
@@ -1,12 +1,11 @@
 module "management" {
   source                = "infrahouse/service-network/aws"
-  version               = "~> 3.0"
+  version               = "3.1.0"
   management_cidr_block = "10.0.0.0/22"
   service_name          = "management"
   vpc_cidr_block        = "10.0.0.0/22"
   environment           = local.environment
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  enable_vpc_flow_logs  = false
   subnets = [
     {
       cidr                    = "10.0.0.0/24"
@@ -26,7 +25,7 @@ module "management" {
       cidr                    = "10.0.2.0/24"
       availability-zone       = data.aws_availability_zones.uw1.names[1]
       map_public_ip_on_launch = true
-      create_nat              = true
+      create_nat              = false
       forward_to              = ""
     },
     {
