cc6c83d0a13979755a39c55f3a7f484e23038492
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-05-18 18:13:15 -0700

    Remove one NAT gateway (#162)
    * Remove one NAT gateway
    
    * Upgrade service network to 3.1.1
    
    * empty sting in forward_to is not allowed anymore
    
    * Route traffic to single NAT
    
    Now, traffic from 10.0.3.0/24 to outside world is routed to the public
    network where the only NAT gateway is.
    
    * Pin actions-runner so we don't recreate it in this PR

--- service-network.tf
+++ service-network.tf
@@ -1,40 +1,32 @@
 module "management" {
   source                = "infrahouse/service-network/aws"
-  version               = "~> 3.0"
+  version               = "3.1.1"
   management_cidr_block = "10.0.0.0/22"
   service_name          = "management"
   vpc_cidr_block        = "10.0.0.0/22"
   environment           = local.environment
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  enable_vpc_flow_logs  = false
   subnets = [
     {
       cidr                    = "10.0.0.0/24"
       availability-zone       = data.aws_availability_zones.uw1.names[0]
       map_public_ip_on_launch = true
       create_nat              = true
-      forward_to              = ""
     },
     {
-      cidr                    = "10.0.1.0/24"
-      availability-zone       = data.aws_availability_zones.uw1.names[0]
-      map_public_ip_on_launch = false
-      create_nat              = false
-      forward_to              = "10.0.0.0/24"
+      cidr              = "10.0.1.0/24"
+      availability-zone = data.aws_availability_zones.uw1.names[0]
+      forward_to        = "10.0.0.0/24"
     },
     {
       cidr                    = "10.0.2.0/24"
       availability-zone       = data.aws_availability_zones.uw1.names[1]
       map_public_ip_on_launch = true
-      create_nat              = true
-      forward_to              = ""
     },
     {
-      cidr                    = "10.0.3.0/24"
-      availability-zone       = data.aws_availability_zones.uw1.names[1]
-      map_public_ip_on_launch = false
-      create_nat              = false
-      forward_to              = "10.0.2.0/24"
+      cidr              = "10.0.3.0/24"
+      availability-zone = data.aws_availability_zones.uw1.names[1]
+      forward_to        = "10.0.0.0/24"
     }
   ]
 }
