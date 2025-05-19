a7ca3da6a560371e08dd2e783ab3034ee6f02801
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-05-18 17:59:30 -0700

    Upgrade service network to 3.1.1

--- service-network.tf
+++ service-network.tf
@@ -1,6 +1,6 @@
 module "management" {
   source                = "infrahouse/service-network/aws"
-  version               = "3.1.0"
+  version               = "3.1.1"
   management_cidr_block = "10.0.0.0/22"
   service_name          = "management"
   vpc_cidr_block        = "10.0.0.0/22"
@@ -12,28 +12,22 @@ module "management" {
       availability-zone       = data.aws_availability_zones.uw1.names[0]
       map_public_ip_on_launch = true
       create_nat              = true
-      forward_to              = ""
     },
     {
-      cidr                    = "10.0.1.0/24"
-      availability-zone       = data.aws_availability_zones.uw1.names[0]
-      map_public_ip_on_launch = false
-      create_nat              = false
-      forward_to              = "10.0.0.0/24"
+      cidr              = "10.0.1.0/24"
+      availability-zone = data.aws_availability_zones.uw1.names[0]
+      forward_to        = "10.0.0.0/24"
     },
     {
       cidr                    = "10.0.2.0/24"
       availability-zone       = data.aws_availability_zones.uw1.names[1]
       map_public_ip_on_launch = true
-      create_nat              = false
       forward_to              = ""
     },
     {
-      cidr                    = "10.0.3.0/24"
-      availability-zone       = data.aws_availability_zones.uw1.names[1]
-      map_public_ip_on_launch = false
-      create_nat              = false
-      forward_to              = "10.0.2.0/24"
+      cidr              = "10.0.3.0/24"
+      availability-zone = data.aws_availability_zones.uw1.names[1]
+      forward_to        = "10.0.2.0/24"
     }
   ]
 }
