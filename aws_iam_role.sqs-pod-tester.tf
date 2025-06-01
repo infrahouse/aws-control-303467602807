f5c676e1f5fb19234aab3e51436f012461197bfc
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-06-01 08:12:31 -0700

    Add role sqs-pod-tester

---
+++ aws_iam_role.sqs-pod-tester.tf
@@ -0,0 +1,13 @@
+module "sqs-pod-tester" {
+  source = "./modules/module-tester-role"
+  providers = {
+    aws = aws.aws-303467602807-uw1
+  }
+  gh_org_name = "infrahouse"
+  repo_name   = "terraform-aws-sqs-pod"
+  role_name   = "tcp-pod-tester"
+  trusted_iam_user_arn = {
+    "me" : local.me_arn
+  }
+  grant_admin_permissions = true
+}
