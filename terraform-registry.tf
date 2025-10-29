ba8de7aaacbe45a6d2ef6625e0c084c531bf5470
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-10-29 16:09:08 -0700

    Deploy https://registry.ci-cd.ingfrahouse.com (#220)

---
+++ terraform-registry.tf
@@ -0,0 +1,17 @@
+module "registry" {
+  source  = "infrahouse/registry/aws"
+  version = "0.2.0"
+  providers = {
+    aws = aws.aws-303467602807-uw1
+  }
+  cognito_users = [
+    {
+      email     = "aleks@infrahouse.com"
+      full_name = "Aleks"
+    }
+  ]
+  environment      = local.environment
+  subnets_backend  = module.management.subnet_private_ids
+  subnets_frontend = module.management.subnet_public_ids
+  zone_id          = module.infrahouse_com.infrahouse_zone_id
+}
