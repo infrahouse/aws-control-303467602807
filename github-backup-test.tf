8920576c6423493e72fb353c8172bc4c532ee623
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2026-02-10 13:46:56 -0800

    Add github-backup-tester role and PEM key secrets across three regions (#237)
    Add a CI tester role for terraform-aws-github-backup and create
      GitHub Backup App PEM key secrets in us-west-1 (source of truth),
      us-west-2, and us-east-1. Add us-west-2 provider alias.

---
+++ github-backup-test.tf
@@ -0,0 +1,43 @@
+module "github_backup_pem" {
+  source             = "registry.infrahouse.com/infrahouse/secret/aws"
+  version            = "~> 1.1"
+  environment        = local.environment
+  service_name       = "github-backup-test"
+  secret_description = "GitHub Backup App PEM key for CI testing"
+  secret_name_prefix = "github-backup-test-pem"
+  readers            = [module.ci-tester["github-backup-tester"].role_arn]
+  writers            = [local.me_arn]
+  providers = {
+    aws = aws.aws-303467602807-uw1
+  }
+}
+
+module "github_backup_pem_uw2" {
+  source             = "registry.infrahouse.com/infrahouse/secret/aws"
+  version            = "~> 1.1"
+  environment        = local.environment
+  service_name       = "github-backup-test"
+  secret_description = "GitHub Backup App PEM key for CI testing"
+  secret_name_prefix = "github-backup-test-pem"
+  secret_value       = module.github_backup_pem.secret_value
+  readers            = [module.ci-tester["github-backup-tester"].role_arn]
+  writers            = [local.me_arn]
+  providers = {
+    aws = aws.aws-303467602807-uw2
+  }
+}
+
+module "github_backup_pem_ue1" {
+  source             = "registry.infrahouse.com/infrahouse/secret/aws"
+  version            = "~> 1.1"
+  environment        = local.environment
+  service_name       = "github-backup-test"
+  secret_description = "GitHub Backup App PEM key for CI testing"
+  secret_name_prefix = "github-backup-test-pem"
+  secret_value       = module.github_backup_pem.secret_value
+  readers            = [module.ci-tester["github-backup-tester"].role_arn]
+  writers            = [local.me_arn]
+  providers = {
+    aws = aws.aws-303467602807-ue1
+  }
+}
