4d3cdfefd64ff09ef0aacf1bd80e7b667c132ef0
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2026-02-10 13:59:36 -0800

    fix: writer role for github app PEM (#238)
    * fix: writer role for github app PEM
    
    * Delete out
    
    ---------
    
    Co-authored-by: infrahouse8 <121983874+infrahouse8@users.noreply.github.com>

--- github-backup-test.tf
+++ github-backup-test.tf
@@ -6,7 +6,7 @@ module "github_backup_pem" {
   secret_description = "GitHub Backup App PEM key for CI testing"
   secret_name_prefix = "github-backup-test-pem"
   readers            = [module.ci-tester["github-backup-tester"].role_arn]
-  writers            = [local.me_arn]
+  writers            = tolist(data.aws_iam_roles.sso-admin.arns)
   providers = {
     aws = aws.aws-303467602807-uw1
   }
@@ -21,7 +21,7 @@ module "github_backup_pem_uw2" {
   secret_name_prefix = "github-backup-test-pem"
   secret_value       = module.github_backup_pem.secret_value
   readers            = [module.ci-tester["github-backup-tester"].role_arn]
-  writers            = [local.me_arn]
+  writers            = tolist(data.aws_iam_roles.sso-admin.arns)
   providers = {
     aws = aws.aws-303467602807-uw2
   }
@@ -36,7 +36,7 @@ module "github_backup_pem_ue1" {
   secret_name_prefix = "github-backup-test-pem"
   secret_value       = module.github_backup_pem.secret_value
   readers            = [module.ci-tester["github-backup-tester"].role_arn]
-  writers            = [local.me_arn]
+  writers            = tolist(data.aws_iam_roles.sso-admin.arns)
   providers = {
     aws = aws.aws-303467602807-ue1
   }
